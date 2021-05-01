e79118c4ab544f2e649b429bd02fcce4afa37caf
a0916105 <whdofjew@gmail.com>
2021-05-01 09:55:06 +0800

    viewModelScope

--- app/src/main/java/tw/idv/jew/coroutinesample/MyViewModel.kt
+++ app/src/main/java/tw/idv/jew/coroutinesample/MyViewModel.kt
@@ -1,7 +1,13 @@
 package tw.idv.jew.coroutinesample
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 class MyViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    init {
+        viewModelScope.launch {
+            //Call suspend function here
+        }
+    }
 }
