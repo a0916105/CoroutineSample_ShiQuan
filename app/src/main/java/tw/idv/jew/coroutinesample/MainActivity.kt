2d6e944508c632bec1790745242d7a48e80744b5
a0916105 <whdofjew@gmail.com>
2021-05-01 10:09:19 +0800

    Cancel與Coroutine停止檢查

--- app/src/main/java/tw/idv/jew/coroutinesample/MainActivity.kt
+++ app/src/main/java/tw/idv/jew/coroutinesample/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +22,27 @@ class MainActivity : AppCompatActivity() {
         lifecycleScope.launch {
             //Call suspend function here
         }
+
+        //Cancel
+        val job = GlobalScope.launch {
+            updateWithDelay()
+        }
+        //Call to stop the Coroutine
+        job.cancel()
+        //用isActive檢查Coroutine是否停止
+        GlobalScope.launch {
+            if (isActive){
+                someCalculation()
+            }
+        }
+        //用try/catch避免Coroutine停止造成Exception
+        GlobalScope.launch {
+            try {
+                someCalculation()
+            }finally {
+                //release your resource here
+            }
+        }
     }
 
     suspend fun updateWithDelay(){
@@ -36,5 +58,9 @@ class MainActivity : AppCompatActivity() {
     private fun updateWithoutDelay() {
         update()
         println("updateWithoutDelay")
+    }
+
+    private fun someCalculation() {
+        println("someCalculation")
     }
 }
